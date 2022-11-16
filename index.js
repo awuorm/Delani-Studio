5dc937b9b2f7d4fc128d9e7eacb828b69f03adcb
awuorm <otieno.awuor.m@gmail.com>
2022-11-16 20:25:07 +0300

    added hover function

---
+++ index.js
@@ -0,0 +1,30 @@
+function onHover()
+{
+    $("#service-icon1").attr('src', './assets/services_icons/design.png');
+}
+
+function offHover()
+{
+    $("#service-icon1").attr('src', './assets/services_icons/design_icon.png');
+}
+
+function onHover2()
+{
+    $("#service-icon2").attr('src', './assets/services_icons/development.png');
+}
+
+function offHover2()
+{
+    $("#service-icon2").attr('src', './assets/services_icons/dev_icon.png');
+}
+
+function onHover3()
+{
+    $("#service-icon3").attr('src', './assets/services_icons/product_mgnt.png');
+}
+
+function offHover3()
+{
+    $("#service-icon3").attr('src', './assets/services_icons/product_icon.png');
+}
+
